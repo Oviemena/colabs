94f15a53589936091ded478ab57d2da78ff7b53a
Oviemena Okpowodu <okpowoduoviemena999@gmail.com>
2024-07-25 12:48:56 +0100

    change the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,38 @@
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
-      <Link href='/' className="md:flex-1">
-        <Image 
-          src="/assets/icons/logo.svg"
-          alt="Logo with name"
-          width={120}
-          height={32}
-          className="hidden md:block"
-        />
-        <Image 
+      <Link href="/" className="md:flex-1">
+        <div className="flex">
+          <Image
+            src="/assets/icons/logo-icon.svg"
+            alt="Logo"
+            width={32}
+            height={32}
+            className="hidden md:block"
+          />
+          <Image
+            src="/assets/icons/colabs.svg"
+            alt="Logo with name"
+            width={120}
+            height={32}
+            className="hidden md:block -mt-1"
+          />
+        </div>
+        <Image
           src="/assets/icons/logo-icon.svg"
           alt="Logo"
           width={32}
           height={32}
-          className="mr-2 md:hidden"
+          className="mr-1 md:hidden"
         />
       </Link>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
